4fafe622659481a9b2d09f063e6cdb0f73985fd9
high-u <zen.crazyd@gmail.com>
2017-09-26 02:16:40 +0900

    iroiro

--- manage-flow-by-git.js
+++ manage-flow-by-git.js
@@ -1,12 +1,9 @@
 module.exports = function (RED) {
-    function gitNode(config) {
-        RED.nodes.createNode(this, config);
-        this.target = config.target;
+    "use strict";
+    function gitNode(n) {
+        RED.nodes.createNode(this, n);
         var node = this;
         this.on('input', function (msg) {
-            var s = JSON.stringify(node);
-            //var s = "s";
-            msg[node.target] = s;
             node.send(msg);
         });
     }
