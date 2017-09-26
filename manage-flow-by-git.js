7b1fedad90a10609f7bde7a696ede514d90da5a9
high-u <zen.crazyd@gmail.com>
2017-09-26 13:03:39 +0900

    a little modification

--- manage-flow-by-git.js
+++ manage-flow-by-git.js
@@ -1,11 +1,10 @@
 module.exports = function (RED) {
-    "use strict";
-    function gitNode(n) {
-        RED.nodes.createNode(this, n);
+    function manageflowbygitNode(config) {
+        RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function (msg) {
             node.send(msg);
         });
     }
-    RED.nodes.registerType('manage-flow-by-git', gitNode);
+    RED.nodes.registerType('manage-flow-by-git', manageflowbygitNode);
 };
